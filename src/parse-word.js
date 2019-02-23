fbefd615f818709301aaaed407fb9ee71714221b
Hsuan Lee <HsuanGM@gmail.com>
2019-02-24 01:52:19 +0800

    pref: performance

--- src/parse-word.js
+++ src/parse-word.js
@@ -1,3 +1,5 @@
+const SpellChecker = require('spellchecker');
+
 const exclude = 'the,to,is,and,of,in,for,var,be,you,on,are,can,an,as,or,we,td,if,tr,by,vue,react,angular,it,js,css,html,' +
     'id,value,title,node,this,const,string,antd,data,change,type,function,set,get,key,false,jsx,button,input,icon,name,select' +
     'item,style,ant,number,fix,date,return,get,class,index,true,list,table,keys,components,text,use,not,fixed,color,show,' +
@@ -5,7 +7,7 @@ const exclude = 'the,to,is,and,of,in,for,var,be,you,on,are,can,an,as,or,we,td,if
     'left,issue,all,event,tag,length,tabs,used,const,config,expanded,box,info,has,sub,let,code,icons,yarn' +
     'array,subtitle,url,end,start,body,html,npm,node,link,webpack,lib,src,but,next,ref,project,test,' +
     'min,max,dot,now,bug,clicked,zhejiang,type,types,dev,hangzhou,err,error,svg,diff,map,error,errors,' +
-    'save,run,javascript,typescript,iconfont,root,num,value,file,size,'.split(',');
+    'save,run,javascript,typescript,iconfont,root,num,value,file,size,git,window,github'.split(',');
 
 /**
  * 清除无效字符串
@@ -17,8 +19,8 @@ function cleanStr(text) {
     text = text.replace(/\[.*\]\(.*\)/g, ''); // md链接
     text = text.replace(/[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/g, ''); // 链接
     text = text.replace(/[0-9]/g, ''); // 数字
-    text = text.replace(/(_|\-)/g, ' '); // 减号和下划线分词
     text = text.replace(/(<.*>)/g, ' '); // html标签
+    text = text.replace(/(_|\-)/g, ' '); // 减号和下划线分词
     text = text.replace(/([A-Z][a-z]*)/g, a => ` ${a.toLowerCase()}`); // 拆分驼峰命名
     text = text.replace(/(?<![aei])([ie][d])(?=[^a-zA-Z])|(?<=[ertkgwmnl])s(?=[^a-zA-Z])/g, a => ''); // 移除复数
     return text.match(/(\w+)/g);
@@ -47,28 +49,19 @@ module.exports = {
      */
     wordFrequency(wordArr) {
         wordArr = wordArr.sort(); // 先进行排序，方便稍后的算法
-        let only = [ ...new Set(wordArr) ]; // 去重
-        
+        let prev;
         let result = [];
-        
-        for (let i = 0; i < only.length; i++) {
-            
-            let wordItem = {name: only[ i ], value: 0};
-            
-            for (let y = 0; y < wordArr.length; y++) {
-                if (wordItem.name === wordArr[ y ]) {
-                    wordItem.value++
-                } else {
-                    wordArr.splice(0, y + 1);
-                    break;
-                }
+    
+        for (let i = 0; i < wordArr.length; i++) {
+            if (wordArr[i] !== prev) {
+                result.push({name: wordArr[ i ], value: 1})
+            } else {
+                result[result.length - 1].value = result[result.length - 1].value + 1;
             }
-            
-            // 排除出现率小于2的单词
-            wordItem.value > 2 && result.push(wordItem);
+            prev = wordArr[i];
         }
-        
+    
         // 返回按词频排序的结果
-        return result.sort((a, b) => b.value - a.value)
+        return result.sort((a, b) => b.value - a.value).filter(e => (e.value > 3 && SpellChecker.isMisspelled(e.name)))
     }
 };
